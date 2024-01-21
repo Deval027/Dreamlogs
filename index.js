1021bf1116fc8f00d5e9cd9babda659e6b6b65a6
deval027 <homeroand6@gmail.com>
2024-01-20 18:01:43 -0600

    Font add on

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const fs = require('fs');
+const { dirname } = require('path');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static('public'));
+
+// Set the proper MIME type for CSS files
+app.use((req, res, next) => {
+  if (req.url.endsWith('.css')) {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
+
 app.get('/', (req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {
+    fs.readFile('views/index.html', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error reading file');
