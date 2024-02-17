26b0a448e4f4509ea004dc53c77aa2d5348acf0d
deval027 <homeroand6@gmail.com>
2024-02-17 13:11:42 -0600

    q

--- public/js/inputs.js
+++ public/js/inputs.js
@@ -2,6 +2,6 @@ fetch('/api/userId')
   .then(response => response.json())
   .then(data => {
     const userId = data.userId;
-    console.log(userId); // or do something else with the user ID
+    console.log(userId); 
   });
 
